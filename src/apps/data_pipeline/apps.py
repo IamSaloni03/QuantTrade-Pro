6b265f3d1a3016df8b20739dbc7134746c08cb88
soniv <sonivasvi5@gmail.com>
2025-11-13 12:37:08 +0530

    created NewsFeed and MarketData models

--- src/apps/data_pipeline/apps.py
+++ src/apps/data_pipeline/apps.py
@@ -1,7 +1,6 @@
 from django.apps import AppConfig
 
-
 class DataPipelineConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'src.apps.data_pipeline'
-    verbose_name = 'Data Pipeline'
+    label = 'quant_data_pipeline'
